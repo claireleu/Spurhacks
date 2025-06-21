913787bb59c273aec864e3b225ae2a9fa1a9e815
elphoun <kale.mzhang7132@gmail.com>
2025-06-21 13:10:11 -0400

    Rush

--- src/pages/Rush.jsx
+++ src/pages/Rush.jsx
@@ -3,8 +3,8 @@ import { BackgroundContainer } from "./components"
 function Rush() {
     return (
         <BackgroundContainer>
-            <div className="h-full w-full rounded-3xl bg-white ring-2">
-                Hello
+            <div className="h-[85vh] w-full rounded-3xl bg-white/50 p-5 shadow-[0px_0px_20px_0.3px_black]">
+                
             </div>
         </BackgroundContainer>
     )
--- src/pages/components.jsx
+++ src/pages/components.jsx
@@ -2,7 +2,7 @@ import background from "../assets/background.png";
 
 const BackgroundContainer = ({ children }) => {
     return (
-        <div className="min-h-screen w-full flex flex-col items-center justify-center select-none p-5"
+        <div className="min-h-screen w-full flex flex-col items-center justify-center select-none py-5 px-10"
             style={{ backgroundImage: `url(${background})` }}>
             {children}
         </ div>
